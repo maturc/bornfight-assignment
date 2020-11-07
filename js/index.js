d14e647ae65245eec84afdc573fb2f54210b86b4
Matija Turčec <turcec.matija@gmail.com>
2020-11-07 09:41:50 +0100

    made the code modular

--- js/index.js
+++ js/index.js
@@ -1,60 +1,9 @@
 const albumList = document.querySelector(".albums");
 
-const renderTemplate = async () => {
-  const albumsUri  = "albums";
-  const artistsUri = "artists";
-  let artists;
-  let albums;
-  await fetchData(artistsUri)
-    .then( data => {
-      artists = data;
-    });
-  await fetchData(albumsUri)
-    .then( data => {
-      albums = data;
-    });
-
-  const template = albums.map( album => {
-    const artistName = artists.filter( artist => artist.id === album.artistId)[0].title;
-    const releaseDate = new Date(album.releaseDate).getFullYear();
-    return `
-      <div class="album">
-        <div class="album__main">
-          <div>
-            <img src="${album.imageUrl}" alt="${album.title}" class="album__img"/>
-            <img src="icons/favorite.svg" class="album__favorite-icon">
-          </div>
-          <div class="album__title">
-            <h2>${album.title}</h2>
-            <h3>${artistName}</h3>
-          </div>
-        </div>
-        <div class="album__details">
-          <div class="album__release-date">
-            <span class="album__released">Relesed:</span>
-            <p>${releaseDate}<p>
-          </div>
-          <span>${album.price}</span>
-          <button class="album__button">
-            Mark as favorite
-          </button>
-        </div>
-      </div>
-    `;
-  }).join("");
-  
-  albumList.innerHTML = template;
+const render = async () => {
+  const [albums, artists] = await fetchAlbumAndArtistData();
+  renderData(albums, artists);
 }
 
 
-// const fetchData = async (query) => {
-//   try {
-//     const uri = "http://localhost:3004/";
-//     const response = await fetch(uri+query);
-//     return await response.json();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-window.addEventListener('DOMContentLoaded', () => renderTemplate());
+window.addEventListener('DOMContentLoaded', () => render());
--- js/shared.js
+++ js/shared.js
@@ -1,9 +1,50 @@
-const fetchData = async (query) => {
+const fetchAlbumAndArtistData = async (query="") => {
   try {
     const uri = "http://localhost:3004/";
-    const response = await fetch(uri+query);
-    return await response.json();
+    const albumsRoute  = "albums/";
+    const artistsRoute = "artists/";
+
+    const albumsRes = await fetch(uri+albumsRoute+query);
+    const albums = await albumsRes.json();
+
+    const artistsRes = await fetch(uri+artistsRoute);
+    const artists = await artistsRes.json();
+
+    return [albums, artists];
   } catch (error) {
     console.log(error);
   }
+}
+
+const renderData = (albums, artists) => {
+  const template = albums.map( album => {
+    const artistName = artists.filter( artist => artist.id === album.artistId)[0].title;
+    const releaseDate = new Date(album.releaseDate).getFullYear();
+    return `
+      <div class="album">
+        <div class="album__main">
+          <div>
+            <img src="${album.imageUrl}" alt="${album.title}" class="album__img"/>
+            <img src="icons/favorite.svg" class="album__favorite-icon">
+          </div>
+          <div class="album__title">
+            <h2>${album.title}</h2>
+            <h3>${artistName}</h3>
+          </div>
+        </div>
+        <div class="album__details">
+          <div class="album__release-date">
+            <span class="album__released">Relesed:</span>
+            <p>${releaseDate}<p>
+          </div>
+          <span>${album.price}</span>
+          <button class="album__button">
+            Mark as favorite
+          </button>
+        </div>
+      </div>
+    `;
+  }).join("");
+  
+  albumList.innerHTML = template;
 }

759650efd81bba63a39407ca9638f0adbb375392
Matija Turčec <turcec.matija@gmail.com>
2020-11-07 14:33:40 +0100

    added favorites feature and error handling

--- js/shared.js
+++ js/shared.js
@@ -1,3 +1,9 @@
+//function index
+//..........................
+//fetchAlbumAndArtistData
+//handleFavoriteButton
+//renderAlbums
+
 const fetchAlbumAndArtistData = async (query="") => {
   try {
     const uri = "http://localhost:3004/";
@@ -13,38 +19,64 @@ const fetchAlbumAndArtistData = async (query="") => {
     return [albums, artists];
   } catch (error) {
     console.log(error);
+    return [null, null];
   }
 }
 
-const renderData = (albums, artists) => {
-  const template = albums.map( album => {
-    const artistName = artists.filter( artist => artist.id === album.artistId)[0].title;
-    const releaseDate = new Date(album.releaseDate).getFullYear();
-    return `
-      <div class="album">
-        <div class="album__main">
-          <div>
-            <img src="${album.imageUrl}" alt="${album.title}" class="album__img"/>
-            <img src="icons/favorite.svg" class="album__favorite-icon">
+const handleFavoriteButton = async (id, isFavorite) => {
+  try {
+    await fetch("http://localhost:3004/albums/" + id, {
+      method: "PATCH",
+      body: JSON.stringify({"favorite": !isFavorite}),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    });
+    render();
+  }
+  catch (error) {
+    console.log(error);
+    alert("Error: Couldn't connect to server!");
+  }
+}
+
+const renderAlbums = (albums, artists) => {
+  let template;
+  //error check
+  if(albums && artists)
+    template = albums.map( album => {
+      const artistName  = artists.filter( artist => artist.id === album.artistId)[0].title;
+      const favorite    = album.favorite ? '<img src="icons/favorite.svg" class="album__favorite-icon">' : '';
+      const releaseDate = new Date(album.releaseDate).getFullYear();
+      return `
+        <div class="album">
+          <div class="album__main">
+            <div>
+              <img src="${album.imageUrl}" alt="${album.title}" class="album__img"/>
+              ${favorite}
+            </div>
+            <div class="album__title">
+              <h2>${album.title}</h2>
+              <h3>${artistName}</h3>
+            </div>
           </div>
-          <div class="album__title">
-            <h2>${album.title}</h2>
-            <h3>${artistName}</h3>
+          <div class="album__details">
+            <div class="album__release-date">
+              <span class="album__released">Relesed:</span>
+              <p>${releaseDate}<p>
+            </div>
+            <span>${album.price}</span>
+            <button class="album__button" onclick="handleFavoriteButton(${album.id}, ${album.favorite})">
+              Mark as favorite
+            </button>
           </div>
         </div>
-        <div class="album__details">
-          <div class="album__release-date">
-            <span class="album__released">Relesed:</span>
-            <p>${releaseDate}<p>
-          </div>
-          <span>${album.price}</span>
-          <button class="album__button">
-            Mark as favorite
-          </button>
-        </div>
+      `;
+    }).join("");
+  else
+    template = `
+      <div class="error">
+        <h1>Error: Couldn't connect to server!</h1>
       </div>
     `;
-  }).join("");
   
   albumList.innerHTML = template;
 }

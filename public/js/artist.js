44e5d04a671e7c7cce56f51065c8c37adf0c69e5
Matija Turčec <turcec.matija@gmail.com>
2020-11-08 10:05:41 +0100

    some code cleanup, removing cLog, changed a let to const

--- public/js/artist.js
+++ public/js/artist.js
@@ -1,16 +1,16 @@
 const albumList = document.querySelector(".albums");
+//artist/:id
 let id = window.location.href;
 id = id.match('[^artist/]*$')[0];
 
 const render = async () => {
-  console.log("artist page");
   const [albums, artists] = await fetchAlbumAndArtistData();
-  let filteredAlbums;
-  filteredAlbums = albums.filter( album => album.artistId == id);
+  const filteredAlbums = albums.filter( album => album.artistId == id);
   renderAlbums(filteredAlbums, artists);
 
+  //change header text to artist name
   const artistNameH1 = document.querySelector("#artist-name");
-  const currentArtist = artists.filter( artist => artist.id == id)
+  const currentArtist = artists.filter( artist => artist.id == id);
   artistNameH1.innerHTML = currentArtist[0].title;
 }
 

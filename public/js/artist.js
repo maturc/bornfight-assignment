97aac1d438c39b0b7c95fdbfb6c835bbf34c3917
Matija Turčec <turcec.matija@gmail.com>
2020-11-08 01:10:55 +0100

    added artist name on artist page

--- public/js/artist.js
+++ public/js/artist.js
@@ -8,6 +8,10 @@ const render = async () => {
   let filteredAlbums;
   filteredAlbums = albums.filter( album => album.artistId == id);
   renderAlbums(filteredAlbums, artists);
+
+  const artistNameH1 = document.querySelector("#artist-name");
+  const currentArtist = artists.filter( artist => artist.id == id)
+  artistNameH1.innerHTML = currentArtist[0].title;
 }
 
 window.addEventListener("DOMContentLoaded", () => render());

505b3d34ce874057e6e8bb0a756cb16a9cdba28a
Matija Turčec <turcec.matija@gmail.com>
2020-11-21 12:53:19 +0100

    some preliminary changes

--- src/artist.ts
+++ src/artist.ts
@@ -1,16 +1,18 @@
-const albumList = document.querySelector(".albums");
+import fetchAlbumAndArtistData from "./utils/fetchData";
+import renderAlbums from "./utils/renderAlbums";
+
 //artist/:id
-let id = window.location.href;
-id = id.match('[^artist/]*$')[0];
+const id: number = Number(window.location.href.match('[^artist/]*$')[0]);
 
 const render = async () => {
+  const albumList = document.querySelector(".albums");
   const [albums, artists] = await fetchAlbumAndArtistData();
-  const filteredAlbums = albums.filter( album => album.artistId == id);
-  renderAlbums(filteredAlbums, artists);
+  const filteredAlbums = albums.filter( album => album.artistId === id);
+  renderAlbums(filteredAlbums, artists, albumList);
 
   //change header text to artist name
   const artistNameH1 = document.querySelector("#artist-name");
-  const currentArtist = artists.filter( artist => artist.id == id);
+  const currentArtist = artists.filter( artist => artist.id === id);
   artistNameH1.innerHTML = currentArtist[0].title;
 }
 

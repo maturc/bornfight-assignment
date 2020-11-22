a9dc3b13511a55608a3c249f7223a2e8ee27d659
Matija Turčec <turcec.matija@gmail.com>
2020-11-22 15:14:07 +0100

    moddiefied to fit new architecture

--- src/artist.ts
+++ src/artist.ts
@@ -1,19 +1,21 @@
+import { IArtists } from "./types";
 import fetchAlbumAndArtistData from "./utils/fetchData";
 import renderAlbums from "./utils/renderAlbums";
 
-//artist/:id
-const id: number = Number(window.location.href.match('[^artist/]*$')[0]);
-
-const render = async () => {
+const init = async () => {
+  //artist/:id
+  const id: number = Number(window.location.href.match('[^artist/]*$')[0]);
   const albumList = document.querySelector(".albums");
+  
   const [albums, artists] = await fetchAlbumAndArtistData();
   const filteredAlbums = albums.filter( album => album.artistId === id);
   renderAlbums(filteredAlbums, artists, albumList);
+  changeHeaderText(id, artists);
+}
+window.addEventListener("DOMContentLoaded", () => init());
 
-  //change header text to artist name
+const changeHeaderText = (id: number, artists: IArtists) => {
   const artistNameH1 = document.querySelector("#artist-name");
   const currentArtist = artists.filter( artist => artist.id === id);
   artistNameH1.innerHTML = currentArtist[0].title;
 }
-
-window.addEventListener("DOMContentLoaded", () => render());
--- src/home.ts
+++ src/home.ts
@@ -1,69 +1,27 @@
-const albumList = document.querySelector(".albums");
-//?limit=limitNum
-let limit = new URLSearchParams(window.location.search).get('limit');
-if(!limit)
-  limit = 10;
-//?q=searchKwd
-const q = new URLSearchParams(window.location.search).get('q');
-let query;
-q ? query = "&q="+q : query ="";
+import fetchAlbumAndArtistData from "./utils/fetchData";
+import renderAlbums from "./utils/renderAlbums";
+import initializeSearchBar from "./utils/searchBar";
 
-const render = async () => {
-  const [albums, artists] = await fetchAlbumAndArtistData("?_limit="+limit+query);
-  renderAlbums(albums, artists);
-  initializeSearchSuggestions(albums);
+const init = async () => {
+  const albumList = document.querySelector(".albums");
+
+  const params: string = getSearchParams();
+  const [albums, artists] = await fetchAlbumAndArtistData("?_limit="+params);
+  renderAlbums(albums, artists, albumList);
+  initializeSearchBar(albums);
 }
+window.addEventListener("DOMContentLoaded", () => init());
 
-window.addEventListener("DOMContentLoaded", () => render());
+const getSearchParams = () => {
+  //?limit=limitNum
+  let limit = new URLSearchParams(window.location.search).get('limit');
+  if(!limit)
+    limit = "10";
+  //?q=searchKwd
+  const q = new URLSearchParams(window.location.search).get('q');
+  let query = "";
+  if(q)
+    query = "&q="+q;
 
-
-//searh bar
-const searchForm  = document.querySelector(".search");
-const searchInput = document.querySelector(".search__input");
-
-const handleSearchSubmit = (e) => {
-  e.preventDefault();
-  window.location = `/?q=${searchInput.value}`;
-}
-searchForm.addEventListener('submit', handleSearchSubmit);
-
-//search bar suggestions
-const initializeSearchSuggestions = (albums) => {
-  const searchBar = document.querySelector(".search__input");
-  searchBar.addEventListener('keyup', (e) => {
-    const searchString = e.target.value.toLowerCase();
-    const filteredAlbums = albums.filter( album => {
-      return album.title.toLowerCase().includes(searchString)
-    });
-    renderSuggestionDropdown(searchBar, filteredAlbums);
-  });
-}
-
-const renderSuggestionDropdown = (searchBar, filteredAlbums) => {
-  suggestionTemplate = filteredAlbums.map( album => {
-    return `
-      <button type="button" onclick="handleSuggestionClick('${album.title}')">
-        ${album.title}
-      </button>
-    `;
-  }).join("");
-
-  const searchSuggestionDropdown = document.querySelector(".search__suggestions");
-  searchSuggestionDropdown.style.width = searchBar.getBoundingClientRect().width+"px";
-
-  if(searchBar.value) {
-    searchSuggestionDropdown.innerHTML = suggestionTemplate;
-    searchSuggestionDropdown.style.display = "flex";
-
-    window.addEventListener('mouseup', (event) => {
-      if (event.target != searchSuggestionDropdown && event.target.parentNode != searchSuggestionDropdown){
-        searchSuggestionDropdown.style.display = 'none';
-        }
-    });
-  }
-  else
-    searchSuggestionDropdown.style.display = "none";
-}
-const handleSuggestionClick = (title) => {
-  window.location = `/?q=${title}`
+  return limit + query;
 }
